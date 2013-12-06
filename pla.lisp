eb841c167beae6aaa48fe60f54b4fb0c1d57309b
mzgcz <mzgcz@self-lisp>
2013-12-06 09:51:10 +0800

    添加零时变量

--- pla.lisp
+++ pla.lisp
@@ -1,6 +1,6 @@
-(defun self-list* (w x)
-  (reduce #'+ 
-	  (mapcar #'* w x)))
+(defun self-list* (x y)
+  (reduce #'+
+	  (mapcar #'* x y)))
 
 (defun self-list+ (x y)
   (mapcar #'+ x y))
@@ -14,12 +14,14 @@
   (let* ((len (list-length (caar dbs)))
 	 (w (make-list len :initial-element 0))
 	 (flag 1))
-    (loop while (= flag 1) do 
-    	 (progn 
+    (loop while (= flag 1) do
+    	 (progn
     	   (setf flag 0)
     	   (dolist (data dbs)
-    	     (when (/= (self-sign (self-list* w (car data))) (cdr data))
-    	       (setf w (self-list+ w (mapcar (lambda (x_i)
-    					       (* x_i (cdr data))) (car data)))
-    		     flag 1)))))
+             (let ((x (car data))
+                   (y (cdr data)))
+               (when (/= (self-sign (self-list* w x)) y)
+                 (setf w (self-list+ w (mapcar (lambda (x_i)
+                                                 (* x_i y)) x))
+                       flag 1))))))
     w))
